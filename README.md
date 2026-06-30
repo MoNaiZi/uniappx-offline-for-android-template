9de987537fcec0e8c9799730ffa876ff68034e65
MoNaiZi <358665514@qq.com>
2026-06-30 15:22:58 +0800

    测试成功，配置国内下载源

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ uni-app x 离线打包 Android 模板项目。
 
 [uni-app x 离线打包示例项目](https://github.com/guodongAndroid/uniappx-offline-demo)
 
-[给前端开发者的 uni-app x 离线打包 Android 详解](https://book.sunxiaodou.com/SourceCodeAnalysis/uni-app-x/0.uni-app-x-offline-packging-for-android.html)
+[给前端开发者的 uni-app x 离线打包 Android 详解](https://book.sunxiaodou.com/SourceCodeAnalysis/uni-app-x/0.uni-app-x-offline-packging-for-android.html)[.gitignore](.gitignore)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,10 @@
 pluginManagement {
     repositories {
+        // 阿里云镜像（Kotlin DSL 用双引号）
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -14,13 +19,14 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/public")
+
         google()
         mavenCentral()
 
-        // for uni-app x
-        maven {
-            url = uri("https://jitpack.io")
-        }
+        // uni-app x 必须保留 jitpack
+        maven("https://jitpack.io")
 
         flatDir {
             dirs("./plugins/")
